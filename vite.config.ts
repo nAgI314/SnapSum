941712e178b087a9dc7890788e98ba29f06674b5
nAgI314 <kurosawasyunnagi@gmail.com>
2024-12-13 13:54:51 +0900

    github_pages2

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/SnapSum/',
+    base: './',
     plugins: [react(), env({ prefix: "VITE", mountedPath: "process.env" })],
     build: {
         rollupOptions: {

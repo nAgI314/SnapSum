fef1685609a80871aa6af9fca561b3a78bec660d
nAgI314 <kurosawasyunnagi@gmail.com>
2024-12-13 13:46:29 +0900

    github_pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import env from "vite-plugin-env-compatible";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
     base: '/SnapSum/',
     plugins: [react(), env({ prefix: "VITE", mountedPath: "process.env" })],
+    build: {
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, 'index.html'),
+            404: resolve(__dirname, '404.html'),
+          }
+        }
+      }
 });

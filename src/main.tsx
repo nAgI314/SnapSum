7c16dd680ee17107d774e7b7f2dfa5bca62e0edd
nAgI314 <kurosawasyunnagi@gmail.com>
2024-12-13 14:59:42 +0900

    github_pages4

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import './index.css'
-import App from './App.tsx'
+import App from "./App.tsx";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router basename="/SnapSum">
+    <Router>
       <App />
     </Router>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
